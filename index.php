1024bc7c4d8161ea5f4e34d9022b3baf69fdef87
tsai <lovez81427@gmail.com>
2023-11-24 10:47:57 +0800

    介紹建構式

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 class Animal{
-    public $name;
+    // public $name;
+    protected $name;
+
+    public function __construct($name) 
+    {
+        $this->name=$name;
+    }
 
     public function setName($name){
         $this->name=$name;
@@ -10,14 +16,14 @@ class Animal{
         return $this->name;
     }
 }
-$animal=new Animal; //實例化
+$animal=new Animal('商品'); //實例化
 
 echo '顯示名稱:'.$animal->getName();
 echo "<br>";
 $animal->setName('佩君');
 echo '顯示名稱:'.$animal->getName();
 echo "<br>";
-$animal->name='哈囉';
-echo '顯示名稱:'.$animal->name;
+// $animal->name='哈囉';
+// echo '顯示名稱:'.$animal->name;
 echo "<br>";
 ?>
