8b094a18e87a99840c4ca06d931e65de4706aa0a
tsai <lovez81427@gmail.com>
2023-11-24 10:41:00 +0800

    類別封裝權限說明

--- index.php
+++ index.php
@@ -6,13 +6,18 @@ class Animal{
     public function setName($name){
         $this->name=$name;
     }
+    public function getName(){
+        return $this->name;
+    }
 }
 $animal=new Animal; //實例化
 
-echo '顯示名稱:'.$animal->name;
+echo '顯示名稱:'.$animal->getName();
 echo "<br>";
 $animal->setName('佩君');
+echo '顯示名稱:'.$animal->getName();
+echo "<br>";
+$animal->name='哈囉';
 echo '顯示名稱:'.$animal->name;
 echo "<br>";
-
 ?>
