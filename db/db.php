51dc89d0d01a2174db333cfac46105d37000f5a9
tsai <lovez81427@gmail.com>
2023-11-30 10:14:24 +0800

    抽離sql_all的語法為獨立的內部函式,增加math()為聚合函數的共用函式

--- db/db.php
+++ db/db.php
@@ -1,8 +1,7 @@
-<?php
+<?php 
 date_default_timezone_set("Asia/Taipei");
 session_start();
-class DB
-{
+class DB{
 
     protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
     protected $pdo;
@@ -10,65 +9,42 @@ class DB
 
     public function __construct($table)
     {
-        $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
     }
 
 
-    function all($where = '', $other = '')
+    function all( $where = '', $other = '')
     {
         $sql = "select * from `$this->table` ";
-
-        if (isset($this->table) && !empty($this->table)) {
-
-            if (is_array($where)) {
-
-                if (!empty($where)) {
-                    $tmp = $this->a2s($where);
-                    $sql .= " where " . join(" && ", $tmp);
-                }
-            } else {
-                $sql .= " $where";
-            }
-
-            $sql .= $other;
-            //echo 'all=>'.$sql;
-            $rows = $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-            return $rows;
-        } else {
-            echo "錯誤:沒有指定的資料表名稱";
-        }
+        $sql =$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-    function count($where = '', $other = '')
-    {
+
+    function count( $where = '', $other = ''){
         $sql = "select count(*) from `$this->table` ";
-
-        if (isset($this->table) && !empty($this->table)) {
-
-            if (is_array($where)) {
-
-                if (!empty($where)) {
-                    $tmp = $this->a2s($where);
-                    $sql .= " where " . join(" && ", $tmp);
-                }
-            } else {
-                $sql .= " $where";
-            }
-
-            $sql .= $other;
-            //echo 'all=>'.$sql;
-            $rows = $this->pdo->query($sql)->fetchColumn();
-            return $rows;
-        } else {
-            echo "錯誤:沒有指定的資料表名稱";
-        }
+        $sql=$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchColumn(); 
     }
-
-
+    private function math($math,$col,$array='',$other=''){
+        $sql="select $math(`$col`)  from `$this->table` ";
+        $sql=$this->sql_all($sql,$array,$other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function sum($col='', $where = '', $other = ''){
+        return  $this->math('sum',$col,$where,$other);
+    }
+    function max($col, $where = '', $other = ''){
+        return  $this->math('max',$col,$where,$other);
+    }  
+    function min($col, $where = '', $other = ''){
+        return  $this->math('min',$col,$where,$other);
+    }  
+    
     function find($id)
     {
         $sql = "select * from `$this->table` ";
-
+    
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= " where " . join(" && ", $tmp);
@@ -81,25 +57,24 @@ class DB
         $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
-
-    function save($array)
-    {
-        if (isset($array['id'])) {
+    
+    function save($array){
+        if(isset($array['id'])){
             $sql = "update `$this->table` set ";
-
+    
             if (!empty($array)) {
                 $tmp = $this->a2s($array);
             } else {
                 echo "錯誤:缺少要編輯的欄位陣列";
             }
-
+        
             $sql .= join(",", $tmp);
             $sql .= " where `id`='{$array['id']}'";
-        } else {
+        }else{
             $sql = "insert into `$this->table` ";
             $cols = "(`" . join("`,`", array_keys($array)) . "`)";
             $vals = "('" . join("','", $array) . "')";
-
+        
             $sql = $sql . $cols . " values " . $vals;
         }
 
@@ -109,7 +84,7 @@ class DB
     function del($id)
     {
         $sql = "delete from `$this->table` where ";
-
+    
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= join(" && ", $tmp);
@@ -119,25 +94,49 @@ class DB
             echo "錯誤:參數的資料型態比須是數字或陣列";
         }
         //echo $sql;
-
+    
         return $this->pdo->exec($sql);
     }
-
-    function q($sql)
-    {
+    
+    /**
+     * 可輸入各式SQL語法字串並直接執行
+     */
+    function q($sql){
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
     }
 
-    
-    private function a2s($array)
-    {
+    private function a2s($array){
         foreach ($array as $col => $value) {
-            $tmp[] = "`$col` = `$value`";
+            $tmp[] = "`$col`='$value'";
         }
         return $tmp;
     }
-}
 
+    private function sql_all($sql,$array,$other){
+
+        if (isset($this->table) && !empty($this->table)) {
+    
+            if (is_array($array)) {
+    
+                if (!empty($array)) {
+                    $tmp = $this->a2s($array);
+                    $sql .= " where " . join(" && ", $tmp);
+                }
+            } else {
+                $sql .= " $array";
+            }
+    
+            $sql .= $other;
+            // echo 'all=>'.$sql;
+            // $rows = $this->pdo->query($sql)->fetchColumn();
+            return $sql;
+        } else {
+            echo "錯誤:沒有指定的資料表名稱";
+        }
+    }
+
+}
 
 function dd($array)
 {
@@ -147,9 +146,18 @@ function dd($array)
 }
 
 
-$student = new DB('students');
-$rows = $student->count();
+$student=new DB('students');
+$rows=$student->count();
 dd($rows);
+echo "<hr>";
+$Score=new DB('student_scores');
+$sum=$Score->sum('score');
+dd($sum);
+echo "<hr>";
+$sum=$Score->sum('score'," where `school_num` <= '911020'");
+dd($sum);
+echo "<hr>";
+$sum=$Score->max('score');
+dd($sum);
 
-// 簡化程式碼
-// 熟悉db裡面全部程式碼,讓自己在半小時內打完這些程式碼
+?>
